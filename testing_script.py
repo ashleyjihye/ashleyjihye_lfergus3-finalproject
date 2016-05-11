70fd679ade1f2caced4f898e63bd44e0bd6cfd6b
lfergus3 <lfergus3@wellesley.edu>
2016-05-11 02:27:53 -0400

    finished testing script

--- testing_script.py
+++ testing_script.py
@@ -3,67 +3,24 @@ import sys
 from subprocess import call
 from subprocess import check_output
 
-"""
-fix:
-    just in case, test whether a lyrics file is empty before doing stuff
-    line after blank (tried making it > instead of >=, broke stuff (the lyrics after a break were missing))
-        -> try appending the line in the else, after the while is done
 
 """
+run on command line with "testing_script testdir"
 
-# run with "testing_script testdir"
+Checks whether a lyrics file is empty, if so ignores the file.
+If a lyrics file is missing a line (ie, there is a note but no lyric) the line is skipped in both the notes and lyrics files.
+"""
 
 if __name__=='__main__':
     testdir = sys.argv[1]
 
     """
-    #use this dict to keep track of whether a song has a lyrics_edited file, ie, whether the song has lyrics
-    lyric_dict = {}JC
-
-    #create a file where each line is the entire lyrics of one testing file
-    with open(os.path.join(testdir, "compositeLyrics"), "w") as lyricsFile:
-        for filename in os.listdir(testdir):
-            if "lyrics_edited" in filename:
-                lyric_dict[filename.split(".")[0]] = filename
-                print filename
-                with open(os.path.join(testdir, filename)) as testFile:
-                    f = testFile.read().split("\n")
-                    syllables_list = []
-                    prev_note = 0
-                    for line in f:
-                        if len(line.split()) == 4:
-                            if int(line.split()[0]) == (prev_note + 1):
-                                syllables_list.append(line.split()[3])
-                                prev_note += 1
-                            else:
-                                #deal with notes that have no lyric
-                                while (int(line.split()[0]) > (prev_note + 1)):
-                                    syllables_list.append("<UNK>")
-                                    prev_note += 1
-                                syllables_list.append(line.split()[3])
-                                prev_note += 1
-
-
-                lyricsFile.write(" ".join(syllables_list) + "\n")
-
-    #create a file where each line is all the notes of one testing file
-    with open(os.path.join(testdir, "compositeNotes"), "w") as notesFile:
-        for filename in os.listdir(testdir):
-            if "notes[Ma7L_edited" in filename:
-                if filename.split(".")[0] in lyric_dict:
-                    print filename
-                    with open(os.path.join(testdir, filename)) as testFile:
-                        f = testFile.read().split("\n")
-                        notes_list = []
-                        for line in f:
-                            if len(line.split()) == 4:
-                                notes_list.append(line.split()[3])
-                    notesFile.write(" ".join(notes_list) + "\n")
+    Creates 2 files in our data directory, one where each line is the lyrics of one song and another where each line
+    contains the corresponding notes. The lyrics file is passed into hmm, and the notes file is used to evaluate the
+    output of hmm.
+    These files only need to be created once, so this code can be commented out if doing several tests in a row with
+    the same song files.
     """
-
-    #use this dict to keep track of whether a song has a lyrics_edited file, ie, whether the song has lyrics
-
-    #create a file where each line is the entire lyrics of one testing file
     with open(os.path.join(testdir, "compositeLyrics"), "w") as lyricsFile:
         with open(os.path.join(testdir, "compositeNotes"), "w") as notesFile:
             for filename in os.listdir(testdir):
@@ -71,34 +28,38 @@ if __name__=='__main__':
                     print filename
                     with open(os.path.join(testdir, filename)) as lyricTestFile:
                         f = lyricTestFile.read().split("\n")
-                        syllables_list = []
-                        notes_list = []
-                        prev_note = 0
-                        for i, lyric_line in enumerate(f):
-                            with open(os.path.join(testdir, (filename.split(".")[0]+".notes_edited.txt"))) as notesTestFile:
-                                notes = notesTestFile.read().split("\n")
-                                if (len(lyric_line.split()) == 4) and (len(notes[int(lyric_line.split("\t")[0])].split("\t")) == 4):
-                                    if int(lyric_line.split()[0]) == (prev_note + 1):
-                                        syllables_list.append(lyric_line.split()[3])
-                                        notes_list.append(notes[int(lyric_line.split("\t")[0])].split()[3])
-                                        prev_note += 1
-                                    else:
-                                        #deal with notes that have no lyric
-                                        while (int(lyric_line.split()[0]) >= (prev_note + 1)):
+                        if len(f) > 1:
+                            syllables_list = []
+                            notes_list = []
+                            prev_note = 0
+                            for i, lyric_line in enumerate(f):
+                                with open(os.path.join(testdir, (filename.split(".")[0]+".notes_edited.txt"))) as notesTestFile:
+                                    notes = notesTestFile.read().split("\n")
+                                    if (len(lyric_line.split()) == 4) and (len(notes[int(lyric_line.split("\t")[0])].split("\t")) == 4):
+                                        if int(lyric_line.split()[0]) == (prev_note + 1):
+                                            syllables_list.append(lyric_line.split()[3])
+                                            notes_list.append(notes[int(lyric_line.split("\t")[0])].split()[3])
                                             prev_note += 1
+                                        else:
+                                            #deal with notes that have no lyric
+                                            while (int(lyric_line.split()[0]) >= (prev_note + 1)):
+                                                prev_note += 1
+                                            syllables_list.append(lyric_line.split()[3])
+                                            notes_list.append(notes[int(lyric_line.split("\t")[0])].split()[3])
                         notesFile.write(" ".join(notes_list) + "\n")
                     lyricsFile.write(" ".join(syllables_list) + "\n")
 
     """
-    #print check_output("python hmm.py " + os.path.join(testdir, "compositeLyrics") + " models/music " + "v", shell=True)
+    Call hmm on the lyrics file created above.
+    """
     call("python hmm.py " + os.path.join(testdir, "compositeLyrics") + " models/music " + "v", shell=True)
 
-    #compare compsiteLyrics.tagged with compositeNotes
-    #keep track of how many notes (accumulate over all files)
-    #keep track of accuracy: for each note, if correct +1, if incorrect stays same
-    #divide accuracy by total notes
-    #print accuracy
 
+    """
+    Calculate and print the accuracy of our hmm compared to the actual notes of the songs.
+    We add one to our count of correct notes for each note that is exactly correct, then divide by the
+    total number of notes. These counts accumulate over all songs in our testing files.
+    """
     total_notes = 0
     correct_notes = 0
     with open(os.path.join(testdir, "compositeNotes")) as gold:
@@ -117,4 +78,3 @@ if __name__=='__main__':
                         continue
     accuracy = correct_notes/(float(total_notes))
     print "The accuracy is: " + str(accuracy)
-    """
